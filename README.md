fb60a0e6df2145e5ea1c7d873bccd0ec08aae0d0
selphaware-ahmadus <uahmad3013@outlook.com>
2021-08-30 12:48:42 +0100

    initial commits

---
+++ README.md
@@ -0,0 +1,8 @@
+# Config (YAML/JSON) Keys Flatten-Reducer
+<i><b>huffpress</b></i> - Python library containing Huffman tree and encoding 
+algorithms. Including a bespoke compressor
+<br><br>
+<b>Installation</b><br>
+````commandline
+pip install huffpress
+````
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="huffpress",
+    version="0.0.1",
+    author="Usman Ahmad",
+    author_email="uahmad3013@outlook.com",
+    description="Library containing Huffman algos and bespoke compressor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/selphaware/huffpress",
+    project_urls={
+        "Bug Tracker": "https://github.com/selphaware/huffpress/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[]
+)
