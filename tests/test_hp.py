3e7f45a29d986368afb9c7e93fe0773505204c4a
selphaware-ahmadus <uahmad3013@outlook.com>
2021-08-31 00:57:22 +0100

    finalising test assertions

--- tests/test_hp.py
+++ tests/test_hp.py
@@ -6,21 +6,21 @@ from os import remove
 
 class TestHuffPress(unittest.TestCase):
     def test_d_txt(self):
-        assert(test_compress("d.txt"), True)
-        remove("d.txt.bak")
-        remove("d.txt.hac")
+        self.assertEqual(test_compress("tests/files/d.txt"), True)
+        remove("tests/files/d.txt.bak")
+        remove("tests/files/d.txt.hac")
 
     def test_i_txt(self):
-        assert(test_compress("i.txt"), True)
-        remove("i.txt.bak")
-        remove("i.txt.hac")
+        self.assertEqual(test_compress("tests/files/i.txt"), True)
+        remove("tests/files/i.txt.bak")
+        remove("tests/files/i.txt.hac")
 
     def test_j_txt(self):
-        assert(test_compress("j.txt"), True)
-        remove("j.txt.bak")
-        remove("j.txt.hac")
+        self.assertEqual(test_compress("tests/files/j.txt"), True)
+        remove("tests/files/j.txt.bak")
+        remove("tests/files/j.txt.hac")
 
     def test_u_exe(self):
-        assert(test_compress("u.exe"), True)
-        remove("u.exe.bak")
-        remove("u.exe.hac")
+        self.assertEqual(test_compress("tests/files/u.exe"), True)
+        remove("tests/files/u.exe.bak")
+        remove("tests/files/u.exe.hac")
