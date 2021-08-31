e12c3a8d3995005ed3d145976efb93c762b73093
selphaware-ahmadus <uahmad3013@outlook.com>
2021-08-31 01:15:57 +0100

    update package ver 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="huffpress",
-    version="0.0.3",
+    version="0.0.4",
     author="Usman Ahmad",
     author_email="uahmad3013@outlook.com",
     description="Library containing Huffman algos and bespoke compressor",
