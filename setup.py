5ed4e8e8b560f0a927c2a4e73a66efe3b7853968
selphaware-ahmadus <uahmad3013@outlook.com>
2021-09-13 16:11:47 +0100

    0.2.9 dispatch overload refactoring

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="huffpress",
-    version="0.2.8",
+    version="0.2.9",
     author="Usman Ahmad",
     author_email="uahmad3013@outlook.com",
     description="Library containing Huffman algos and bespoke compressor",
